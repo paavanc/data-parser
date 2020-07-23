2bf80455e6d572b670907ea844aa5429161f50b5
Paavan Chopra <paavan012chopra@gmail.com>
2020-07-23 11:15:36 -0400

    first commit

---
+++ src/test/java/com/data/inference/InferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.inference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
